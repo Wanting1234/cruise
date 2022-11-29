ec53050167dac1df252a5a48161c00ff694a3691
Ting Wang <ting.wang@thoughtworks.com>
2022-11-29 16:24:46 +0800

    feat: render footer

---
+++ homework/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            © Copyright 2019 <strong>Thought</strong>Works
+        </footer>
+    );
+}
+
+export default Footer
