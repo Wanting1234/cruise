8ddd19644c2c3ab7dfd35cd67ea54c1c322c1c55
Ting Wang <ting.wang@thoughtworks.com>
2022-12-01 00:31:02 +0800

    feat: show menu list when click user list button

--- homework/src/components/Header/Header.js
+++ homework/src/components/Header/Header.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from '../../image/logo/logo.svg'
 import avatar from '../../image/logo/avatar.jpg'
 import './Header.css'
 
 const Header = () => {
+    const [isMenuShowed, setIsMenuShowed] = useState(false);
+    const menu = [
+        { icon: 'icon-id-card', label: 'Profile' },
+        { icon: 'icon-sign-in', label: 'Sign Out' }
+    ]
+
+    function handleClick() {
+        setIsMenuShowed(!isMenuShowed);
+    }
+
     return(
         <header className="header">
             <img src={logo} alt="cruise" className="logo"/>
             <img src={avatar} alt="user-avatar" className="user-avatar"/>
             <input type="checkbox"
                    className="user-menu-list"
-                   id="user-menu-list"/>
+                   id="user-menu-list"
+                   onClick={handleClick}
+            />
             <label htmlFor="user-menu-list"></label>
+            {!isMenuShowed ?
+                null :
+                <section>
+                    <ul className="header-user-menu-list">
+                        {menu.map((item, index) => (
+                            <li className="user-menu-list-item" key={index}>
+                                <span className={`iconfont ${item.icon}`}></span>
+                                <span className="user-menu-list-item-label">{item.label}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+
+            }
         </header>
     );
 }
