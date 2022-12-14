2f9802f4ffd8e96e1a47b657163f8c5de90c959f
Ting Wang <ting.wang@thoughtworks.com>
2022-12-14 21:55:10 +0800

    feat: set navigation display when hover it

--- homework/src/components/Main/Navigation/Navigation.js
+++ homework/src/components/Main/Navigation/Navigation.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import History from "./History/History";
 import './Navigation.css'
 import {Link} from "react-router-dom";
 
 const Navigation = () => {
     const menu = [
-        { id: '1', icon: 'icon-dashboard', name: 'DASHBOARD' },
-        { id: '2', icon: 'icon-sitemap', name: 'AGENT' },
-        { id: '3', icon: 'icon-boat', name: 'MY CRUISE' },
-        { id: '4', icon: 'icon-life-bouy', name: 'HELP' }
+        {id: '1', icon: 'icon-dashboard', name: 'DASHBOARD'},
+        {id: '2', icon: 'icon-sitemap', name: 'AGENT'},
+        {id: '3', icon: 'icon-boat', name: 'MY CRUISE'},
+        {id: '4', icon: 'icon-life-bouy', name: 'HELP'}
     ];
+    const [active, setActive] = useState('AGENT')
 
-    return(
+    return (
         <div className="navigation">
             <section className="navigation-message">
                 <ul>
-                    {menu.map((item) => <li className="nav" key={item.id}>
-                        <Link to={`${item.name.toLowerCase()}`} className="link">
+                    {menu.map((item) =>
+                        <li className={`nav ${active === item.name ? 'active-li' : ''}`} key={item.id}>
+                        <Link to={`${item.name.toLowerCase()}`}
+                              className={`link ${active === item.name ? 'active-li' : ''}`}
+                              onClick={() => setActive(item.name)}>
                             <span className={`iconfont ${item.icon}`}></span>
                             <span>{item.name}</span>
                         </Link>
                     </li>)}
                 </ul>
             </section>
-            <History />
+            <History/>
         </div>
     );
 }
