3c3cc27fb7a443ed94c9049b6ef11e1812aea4ae
Ting Wang <ting.wang@thoughtworks.com>
2022-12-25 19:28:16 +0800

    refactor: extract a component to simplify AgentInfo component

--- homework/src/components/Main/AgentView/AgentList/AgentItem/AgentDetail/AgentInfo.js
+++ homework/src/components/Main/AgentView/AgentList/AgentItem/AgentDetail/AgentInfo.js
@@ -1,23 +1,18 @@
 import React from "react";
 
-export function AgentInfo(props) {
-    // todo: duplicated code(extract component)
-    // todo: mysterious name
+export function AgentInfo({name, status, ip, location}) {
+
+    const AgentInfoDetail = ({detail, iconType}) => <div className={`${detail}-with-icon`}>
+        <span className={`iconfont icon-${iconType}`}></span>
+        <span className={`${detail === name ? 'name' : null}`}>{detail}</span>
+    </div>;
+
     return <div className="agent-info">
-        <div className="name-with-icon">
-            <span className="iconfont icon-desktop"></span>
-            <span className="name">{props.name}</span>
+        <AgentInfoDetail detail={name} iconType={'desktop'}/>
+        <div className={`info-label ${status === "building" ? "yellow" : "green"}`}>
+            {status}
         </div>
-        <div className={`info-label ${props.status === "building" ? "yellow" : "green"}`}>
-            {props.status}
-        </div>
-        <div className="ip-with-icon">
-            <span className="iconfont icon-info"></span>
-            <span>{props.ip}</span>
-        </div>
-        <div className="location-with-icon">
-            <span className="iconfont icon-folder"></span>
-            <span>{props.location}</span>
-        </div>
+        <AgentInfoDetail detail={ip} iconType={'info'}/>
+        <AgentInfoDetail detail={location} iconType={'folder'}/>
     </div>;
 }
