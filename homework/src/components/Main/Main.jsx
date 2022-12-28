028cf8421b167f135a14ee188ee4d3b340dbe1c7
Ting Wang <ting.wang@thoughtworks.com>
2022-12-28 16:18:10 +0800

    refactor: refactor main component according to eslint settings

---
+++ homework/src/components/Main/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navigation from './Navigation/Navigation';
+import AgentView from './AgentView/AgentView';
+import './Main.css';
+import { OtherView } from './OtherView/OtherView';
+
+function Main() {
+  const navigationState = useSelector((state) => state.navigation.isNavigationShow);
+  return (
+    <BrowserRouter>
+      <main className="main">
+        {navigationState && <Navigation />}
+        <Routes>
+          <Route path="/" element={<AgentView />} />
+          <Route path="/agent" element={<AgentView match="agent" />} />
+          <Route path="/dashboard" element={<OtherView match="dashboard" />} />
+          <Route path="/my cruise" element={<OtherView match="my cruise" />} />
+          <Route path="/help" element={<OtherView match="help" />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
---
+++ homework/src/components/Main/Main.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import Main from './Main';
+import renderWithProviders from '../../test/utils';
+
+describe('Main', () => {
+  test('should render navigation and content', async () => {
+    const promise = Promise.resolve();
+
+    renderWithProviders(<Main />);
+
+    await act(() => promise);
+    expect(await screen.findByRole('main')).toBeInTheDocument();
+    expect(await screen.findByRole('navigation')).toBeInTheDocument();
+    expect(await screen.findByRole('content')).toBeInTheDocument();
+  });
+
+  test('should render navigation and other content page when click DASHBOARD', () => {
+    renderWithProviders(<Main />);
+
+    userEvent.click(screen.queryByText('DASHBOARD'));
+
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.queryByRole('content')).not.toBeInTheDocument();
+    expect(screen.getByRole('content-page')).toBeInTheDocument();
+    expect(screen.getByText('dashboard Loading...')).toBeInTheDocument();
+  });
+});
