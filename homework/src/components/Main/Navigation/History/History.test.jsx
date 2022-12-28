b891130dca8ea55511aedc67e8ecb80c28825c9e
Ting Wang <ting.wang@thoughtworks.com>
2022-12-28 16:22:14 +0800

    refactor: refactor History component according to eslint settings

---
+++ homework/src/components/Main/Navigation/History/History.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import HistoryList from './HistoryList';
+import renderWithProviders from '../../../../test/utils';
+
+describe('history', () => {
+  test('should render history list', () => {
+    renderWithProviders(<HistoryList />);
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(11);
+  });
+});
